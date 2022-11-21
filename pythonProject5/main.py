2e6ce207f1c578e9243b12c62757dc3a8f514655
TorVik247 <promasterbro248@gmail.com>
2022-11-21 23:07:36 +0200

    Update main.py

--- pythonProject5/main.py
+++ pythonProject5/main.py
@@ -8,11 +8,36 @@ AG=[0,1,1,0,0,0,
     0,0,0,1,1,0]
 AG_=[]
 j=int(math.sqrt(len(AG)))
+one=0
+c=0
+k=0
+
+for o in range(6):
+    i=0
+    six=0
+    while i<j:
+        while k<j-1:
+            c = (AG[one] * AG[six]) + c
+            one = one + 1
+            six = six + j
+            k=k+1
+        AG_.append(c)
+        one= one - (j-1)
+        six = six - (((j-1)*j)-1)
+        c = 0
+        k = 0
+        i=i+1
+    one=one +j
+
+
+
+
+
 i=0
-
-while i<len(AG):
-
-    #c=(AG[b]+AG[b1])+c
-    #AG_.append(c)
+k=0
+for k in AG_:
+    if i>j-1:
+        print()
+        i=0
+    print(k, end=" ")
     i=i+1
-print(AG_)
