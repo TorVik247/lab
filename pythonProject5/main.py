82ac0c9bc8db99a84028216562e90afbf96579da
TorVik247 <promasterbro248@gmail.com>
2022-11-22 20:16:31 +0200

    Update main.py

--- pythonProject5/main.py
+++ pythonProject5/main.py
@@ -1,39 +1,58 @@
 import math
 
-AG=[0,1,1,0,0,0,
-    1,0,1,1,1,0,
-    1,1,0,1,1,0,
-    0,1,1,0,1,1,
-    0,1,1,1,0,1,
-    0,0,0,1,1,0]
+AGC=[0, 1, 1, 0, 0, 0,
+     1, 0, 1, 1, 1, 0,
+     1, 1, 0, 1, 1, 0,
+     0, 1, 1, 0, 1, 1,
+     0, 1, 1, 1, 0, 1,
+     0, 0, 0, 1, 1, 0]
 
-AG_=[]
-j=int(math.sqrt(len(AG)))
+AGG=[]
+j=int(math.sqrt(len(AGC)))
 one=0
 c=0
 k=0
 
-for o in range(6):
-    i=0
-    six=0
-    while i<j:
-        while k<j-1:
-            c = (AG[one] * AG[six]) + c
-            one = one + 1
-            six = six + j
-            k=k+1
-        AG_.append(c)
-        one= one - (j-1)
-        six = six - (((j-1)*j)-1)
+
+while True:
+    if (AGC.count(0)<=j):
+        break
+    else:
+        for o in range(j):
+            i = 0
+            six = 0
+            while i < j:
+                while k < j - 1:
+                    c = (AGC[one] * AGC[six]) + c
+                    one = one + 1
+                    six = six + j
+                    k = k + 1
+                AGG.append(c)
+                one = one - (j - 1)
+                six = six - (((j - 1) * j) - 1)
+                c = 0
+                k = 0
+                i = i + 1
+            one = one + j
+        one = 0
         c = 0
         k = 0
-        i=i+1
-    one=one +j
+        o=0
+        i=0
+        six=0
+        for p in range((j*j)-1):
+            if (p==six):
+                six+=j+1
+                continue
+            elif(AGC[p] == 0):
+                AGC.pop(p)
+                AGC.insert(p, AGG[p])
+        AGG.clear()
 
 i=0#нема смисла виводити має бути   Матрицю відстаней
 k=0
-for k in AG_:
-    if i>j-1:
+for k in AGC:
+    if i > j - 1:
         print()
         i=0
     print(k, end=" ")
